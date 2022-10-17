0d33d68fab2c4cfbbeb063a4b651effc613830ee
Arup Mandal <110271890+cyberboyarup@users.noreply.github.com>
2022-10-17 18:24:59 +0530

    Arup Mandal

---
+++ app.js
@@ -0,0 +1,96 @@
+//Importing express and all other modules on top
+const express = require("express");
+const app = express();
+const ejsMate = require("ejs-mate");
+const path = require("path");
+const methodOverride = require("method-override");
+const nodemailer = require('nodemailer');
+const bodyparser = require("body-parser");
+
+
+//User data modules
+const userProjects = require("./userData/projecthelper.js");
+
+//Setting up the view engine and it's directory
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//All Express middleware/ Static files
+app.use(express.urlencoded({ extended: true })); //req.body is parsed as a form
+app.use(methodOverride("_method")); //Setting the query for method-override
+app.use(express.static(path.join(__dirname, "public"))); //It will serve our static files
+
+//Default Index/Home Route
+//We don't need to add backslash when rendering view pages
+app.get("/", async (req, res) => {
+  try {
+    //Fetch the incoming JSON data
+    const data = await JSON.stringify(userProjects);
+    //Parse it into JavaScript Object
+    const projects = await JSON.parse(data);
+    res.render("homepage", { projects });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// Experience Route
+app.get("/experience", (req, res) => {
+  res.render("experience");
+});
+
+// Experience Route
+app.get("/project", async (req, res) => {
+  try {
+    //Fetch the incoming JSON data
+    const data = await JSON.stringify(userProjects);
+    //Parse it into JavaScript Object
+    const projects = await JSON.parse(data);
+    res.render("project", { projects });
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get("/",function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/",function(req, res){
+
+  const comm = req.body.message;
+   const na = req.body.nameofperson;
+  var transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: '',
+        pass: '' 
+      }
+    });
+
+    var mailOptions = {
+      from: '',
+      to: req.body.username,
+      cc: '',
+      subject: 'Thanks for giving feedback ' + na,
+      text: 'Thanks for your message you have sent to us --> ' + comm
+    };
+    
+    transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("sent");
+        res.redirect("/");
+      }
+    });
+    
+    
+  });
+
+//Starting up server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Serving on the port: ${port}`);
+});
---
+++ userData/projecthelper.js
@@ -0,0 +1,63 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Portfolio Website",
+    desc: "My Protfolio Website. A tempting UX/UI inspired by the top Portfolios. You are already there !",
+    image: "/images/projects/Protfolio_Website.jpg",
+    links: {
+      view: "arupmandal.github.io",
+      code: "https://github.com/arupmandal/arupmandal.github.io",
+    },
+  },
+
+  {
+    id: 2,
+    name: "ShareMe",
+    desc: "ShareMe By Arup Mandal !!",
+    image: "/images/projects/shareme.jpg",
+    links: {
+      view: "https://shareme-delta.vercel.app/",
+      code: "https://github.com/cyberboyarup/ShareMe",
+    },
+  },
+  {
+    id: 3,
+    name: "Redirect to Heroku",
+    desc: "Redirect to Heroku",
+    image: "/images/projects/Redirect-to-Heroku.jpg",
+    links: {
+      view: "https://arup-bots.github.io/Redirect-to-Heroku/",
+      code: "https://github.com/Arup-Bots/Redirect-to-Heroku",
+    },
+  },
+  {
+    id: 4,
+    name: "AUpi",
+    desc: "Create and download QR codes for UPI payments.",
+    image: "/images/projects/AUPI.jpg",
+    links: {
+      view: "https://upiqr.vercel.app/",
+      code: "https://github.com/arupmandal/upiqr",
+    },
+  },
+  {
+    id: 5,
+    name: "Weather App",
+    desc: "Weather App    ",
+    image: "/images/projects/Weather.jpg",
+    links: {
+      view: "#",
+      code: "#",
+    },
+  },
+  {
+    id: 6,
+    name: "Music Player",
+    desc: "",
+    image: "/images/projects/Music.jpg",
+    links: {
+      view: "music-player-2-0-psi.vercel.app/",
+      code: "https://github.com/arupmandal/Music-Player-2.0",
+    },
+  },
+];
